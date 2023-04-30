8de6ed94dbd421b0b411904de95ebaf97c443d95
Dmitry Remezov <mysecundant@gmail.com>
2023-05-01 04:09:47 +0600

    chore: upgrade dependencies, add changesets, fix lib generator

---
+++ .changeset/mean-ants-marry.md
@@ -0,0 +1,5 @@
+---
+'@neodx/log': minor
+---
+
+Rework public API, optimize main and platfrom-specific entries, balance built-in defaults
--- tools/scripts/bin.mjs
+++ tools/scripts/bin.mjs
@@ -1,4 +1,5 @@
 import { generateFiles } from '@neodx/codegen';
+import { omit } from '@neodx/std';
 import { createVfs } from '@neodx/vfs';
 import devkit from '@nrwl/devkit';
 import { $ } from 'execa';
@@ -19,7 +20,7 @@ sade('neodx')
 
     await generateFiles(vfs, join(rootDir, 'templates/lib'), `libs/${name}`, { name });
     await vfs.updateJson(`libs/${name}/package.json`, prev => ({
-      ...prev,
+      ...omit(prev, ['private', 'publishConfig']),
       scripts: Object.fromEntries(
         Object.entries(prev.scripts).map(([key, value]) => [
           key,
