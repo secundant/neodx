8fad708189c87e2e19af09a0fd539409401ecfc3
Dmitry Remezov <mysecundant@gmail.com>
2023-05-01 03:33:26 +0600

    feat(log): improve types, extract defaults to shared space

--- libs/log/src/types.ts
+++ libs/log/src/types.ts
@@ -1,5 +1,5 @@
 /**
- * All log methods are implementing this interface.
+ * Representing log methods. All log methods must implement this interface.
  */
 export interface LoggerMethod {
   <T extends object>(target: T, message?: string, ...args: unknown[]): void;
@@ -7,20 +7,35 @@ export interface LoggerMethod {
   (message: string, ...args: unknown[]): void;
 }
 
-export interface LoggerTransformer<Level> {
+/**
+ * Custom transformer function that will receive log chunks before they are passed to streams.
+ */
+export interface LoggerTransformer<Level extends string> {
   (chunk: LogChunk<Level>): LogChunk<Level>;
 }
 
-export interface LoggerStreamTarget<Level> {
+export interface LoggerHandler<Level extends string> {
   (chunk: LogChunk<Level>): void | Promise<void>;
 }
 
-export interface LoggerStream<Level> {
-  level: Level;
-  targets: LoggerStreamTarget<Level>[];
+export interface LoggerHandleConfig<Level extends string> {
+  /**
+   * The minimum level priority that this stream will receive.
+   * @example 'info' - will receive 'info', 'warn' and 'error' chunks
+   * @example 'warn' - will receive 'warn' and 'error' chunks
+   * @example 'error' - will receive only 'error' chunks
+   * @default no minimum level, will receive all chunks
+   */
+  level?: Level;
+  /**
+   * Your handler function(s) that will receive log chunks.
+   * @example (chunk) => console.log(chunk)
+   * @example (chunk) => Promise.resolve(console.log(chunk))
+   */
+  target: LoggerHandler<Level> | LoggerHandler<Level>[];
 }
 
-export interface LogChunk<Level> {
+export interface LogChunk<Level extends string> {
   /**
    * The name of the logger that created this chunk.
    * @example 'my-app'
@@ -38,7 +53,7 @@ export interface LogChunk<Level> {
    */
   level: Level;
   /**
-   * The error that was thrown, should the level be 'error' or 'fatal'.
+   * The error that was passed as first argument to the log method (usually at `error` level).
    */
   error?: Error;
   /**
@@ -50,15 +65,31 @@ export interface LogChunk<Level> {
   /**
    * Pre-formatted message.
    */
-  message: string;
+  msg: string;
+  msgArgs?: unknown[];
+  msgTemplate?: string;
+
+  __: Readonly<LoggerInternals<Level>>;
 }
 
+export interface LoggerInternals<Level extends string> {
+  /**
+   * Dictionary of log levels with priority (lower is more prioritized).
+   * @default { error: 10, warn: 20, info: 30, verbose: 40, debug: 50, silent: Infinity }
+   */
+  levelsConfig: LoggerLevelsConfig<Level>;
+}
+
+export type LoggerLevelsConfig<Level extends string> = Record<Level, number>;
 export type LoggerBaseMeta = Record<keyof any, unknown>;
 export type LoggerMethods<Levels extends string> = Record<Levels, LoggerMethod>;
 
 export type Logger<Levels extends string> = {
-  fork(options?: Partial<LoggerParams<Levels>>): Logger<Levels>;
-  child(name: string, options?: Partial<Omit<LoggerParams<Levels>, 'name'>>): Logger<Levels>;
+  fork<T extends Levels>(options?: Partial<LoggerParams<T>>): Logger<T>;
+  child<T extends Levels>(
+    name: string,
+    options?: Partial<Omit<LoggerParams<T>, 'name'>>
+  ): Logger<T>;
 } & LoggerMethods<Levels>;
 
 export interface LoggerParams<Level extends string> {
@@ -69,30 +100,34 @@ export interface LoggerParams<Level extends string> {
    */
   name: string;
   /**
-   * The logging level, everything below this level will be ignored.
+   * The logging level, everything higher than this level will be ignored.
    * @example 'info'
    * @example 'verbose'
    */
-  level: Level;
+  level?: Level;
   /**
-   * Dictionary of log levels with priority. The higher the number, the more important the level.
+   * Dictionary of log levels with priority (lower is more prioritized).
+   * The higher the number, the less important the level and the more likely it will be ignored.
    * @default { error: 10, warn: 20, info: 30, verbose: 40, debug: 50 }
    * @example { foo: 10, bar: 20, baz: 30 } - custom levels, where 'foo' is the most important and 'baz' is the least important
    */
-  levels: Record<Level, number>;
+  levels: LoggerLevelsConfig<Level>;
   /**
    * Additional fields that will be added to every log chunk.
    */
-  fields: LoggerBaseMeta;
-  // TODO Add support for streams without level (all levels)
+  meta?: LoggerBaseMeta;
   // TODO Add support for streams with multiple levels
   // TODO Add support for streams with min and max levels
-  // TODO Add shorthand for streams with only one target, e.g. streams: [{ level: 'info', target: console.log }]
-  // TODO Add shorthand for streams without level, e.g. streams: [console.log]
   /**
    * List of streams that will receive log chunks.
-   * @example [{ level: 'info', targets: [console.log] }]
+   * @example [{ level: 'info', target: [console.log] }, { level: 'error', target: [console.error] }]
+   * @example [{ level: 'info', target: console.log }, { level: 'error', target: console.error }]
+   * @example [console.log]
+   * @example console.log
+   * @example { level: 'info', target: console.log }
+   * @example { level: 'info', target: [console.log] }
+   * @example { level: 'info', target: [{ write: console.log }] }
    */
-  target: LoggerStream<Level>[];
-  transform: LoggerTransformer<Level>[];
+  target: LoggerHandler<Level> | Array<LoggerHandler<Level> | LoggerHandleConfig<Level>>;
+  transform?: LoggerTransformer<Level> | LoggerTransformer<Level>[];
 }
