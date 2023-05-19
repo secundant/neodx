d96ff04f47e3d87c0da72a516174c325a951e229
Dmitry Remezov <mysecundant@gmail.com>
2023-05-19 12:07:05 +0600

    test(std): fix `concurrent` test

--- libs/std/src/__tests__/concurrent.test.ts
+++ libs/std/src/__tests__/concurrent.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { concurrent } from '../async';
-import { renderWaterfall } from './testing-utils';
+import { createMicroTimer, renderWaterfall } from './testing-utils';
 
 describe('concurrent', () => {
   test('should return empty array on empty inputs', async () => {
@@ -10,26 +10,25 @@ describe('concurrent', () => {
   });
 
   test('should display concurrent execution', async () => {
+    const microTimer = createMicroTimer();
     const waterfall = [] as [number, number, number][];
-    const startTime = Date.now();
-    const time = () => 10 * Math.floor((Date.now() - startTime) / 10);
 
     const run = concurrent(async (input: number) => {
-      const mark = [input, time(), 0] as [number, number, number];
+      const time = input % 3 === 0 ? 40 : input % 2 === 0 ? 30 : 20;
+      const mark = [input, microTimer.passed, 0] as [number, number, number];
 
       waterfall.push(mark);
-      await new Promise(resolve =>
-        setTimeout(resolve, input % 3 === 0 ? 40 : input % 2 === 0 ? 30 : 20)
-      );
-
-      mark[2] = time();
+      mark[2] = await microTimer.wait(time);
       return input * 2;
     }, 3);
 
     const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     const expected = input.map(input => input * 2);
+    const runPromise = run(input);
 
-    expect(await run(input)).toEqual(expected);
+    await microTimer.start();
+
+    expect(await runPromise).toEqual(expected);
     expect(renderWaterfall(waterfall.map(([input, start, end]) => [input, start / 2, end / 2])))
       .toMatchInlineSnapshot(`
       "
--- libs/std/src/__tests__/testing-utils.ts
+++ libs/std/src/__tests__/testing-utils.ts
@@ -6,6 +6,7 @@ export function renderWaterfall<Value>(
 ) {
   // eslint-disable-next-line unicorn/no-unreadable-array-destructuring
   const maxEnd = Math.max(...items.map(([, , end]) => end));
+
   const rendered = items
     .map(([input, start, end]) =>
       [
@@ -19,4 +20,38 @@ export function renderWaterfall<Value>(
     .join('\n');
 
   return ['', rendered, ''].join('\n');
+}
+
+export function createMicroTimer() {
+  let passed = 0;
+  let tasks: [resolve: (value: number) => void, left: number][] = [];
+  const tick = async () => {
+    if (tasks.length === 0) {
+      return;
+    }
+    passed++;
+    for (const task of tasks) {
+      const [resolve, left] = task;
+
+      task[1]--;
+      if (left === 1) {
+        resolve(passed);
+      }
+    }
+    tasks = tasks.filter(([, left]) => left > 0);
+    await new Promise<void>(r => setTimeout(r, 0)); // any timeout
+    await tick();
+  };
+
+  return {
+    get passed() {
+      return passed;
+    },
+    wait(ms: number) {
+      return new Promise<number>(resolve => tasks.push([resolve, ms]));
+    },
+    start() {
+      return tick();
+    }
+  };
 }
