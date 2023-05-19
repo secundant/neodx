ecd237e0360f845093a0ce44644b1c8be614805f
Dmitry Remezov <mysecundant@gmail.com>
2023-05-19 18:36:32 +0600

    feat(svg): add built-in `fixViewBox` plugin for fallback missed viewBox

---
+++ libs/svg/src/plugins/fix-view-box.ts
@@ -0,0 +1,24 @@
+import { createPlugin } from '../plugin-utils';
+
+/**
+ * Fix viewBox attribute if it's missing and width/height are present
+ */
+export const fixViewBox = () =>
+  createPlugin('fix-view-box', {
+    transformNode({ node }) {
+      const {
+        attributes: { viewBox, width, height }
+      } = node;
+
+      if (viewBox || !width || !height) {
+        return node;
+      }
+      return {
+        ...node,
+        attributes: {
+          ...node.attributes,
+          viewBox: `0 0 ${width} ${height}`
+        }
+      };
+    }
+  });
