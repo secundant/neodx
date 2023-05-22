7cdb6b57666d134b3e53ac92c5a0d88513098b39
Dmitry Remezov <mysecundant@gmail.com>
2023-05-22 17:47:32 +0300

    feat(svg): rework `resetColors` plugin

--- libs/svg/src/plugins/index.ts
+++ libs/svg/src/plugins/index.ts
@@ -1,5 +1,12 @@
+export { fixViewBox } from './fix-view-box';
 export { type GroupPluginOptions, groupSprites } from './group';
-export { type ResetColorsPluginOptions, resetColors } from './reset-colors';
+export type {
+  AnyColorInput,
+  ColorPropertyReplacementInput,
+  ColorReplacementInput,
+  ResetColorsPluginParams
+} from './reset-colors';
+export { resetColors } from './reset-colors';
 export { setId } from './set-id';
 export { type SvgoPluginOptions, svgo } from './svgo';
 export { type TypescriptPluginOptions, typescript } from './typescript';
--- libs/svg/src/plugins/reset-colors.ts
+++ libs/svg/src/plugins/reset-colors.ts
@@ -1,52 +1,93 @@
-import { compactObject } from '@neodx/std';
+import { toArray } from '@neodx/std';
+import { type AnyColor, type Colord, colord } from 'colord';
 import { createPlugin } from '../plugin-utils';
 import type { SvgNode } from '../types';
 
-export interface ResetColorsPluginOptions {
-  includeProperties: string[];
-  includeValues: string[];
-  replaceTo: string;
+export type ResetColorsPluginParams =
+  | ColorPropertyReplacementInput
+  | ColorPropertyReplacementInput[];
+
+export interface ColorPropertyReplacementInput {
+  properties?: string | string[];
+  replace?: ColorReplacementInput | ColorReplacementInput[];
+  replaceUnknown?: string;
 }
 
-export const resetColors = (options: Partial<ResetColorsPluginOptions> = {}) => {
-  const mergedOptions = { ...defaults, ...compactObject(options) };
+export interface ColorReplacementInput {
+  from: AnyColorInput | AnyColorInput[];
+  to: string;
+}
+
+export type AnyColorInput = AnyColor | Colord;
+
+export const resetColors = (params: ResetColorsPluginParams = defaults) => {
+  const replacements = toArray(params).map<ColorPropertyReplacement>(input => ({
+    properties: toArray(input.properties ?? defaults.properties),
+    replaceUnknown: input.replaceUnknown,
+    replace: toArray(input.replace ?? []).map(({ from, to }) => ({
+      from: toArray(from).map(colord),
+      to
+    }))
+  }));
 
   return createPlugin('reset-colors', {
     transformNode({ node }) {
       return {
         ...node,
-        children: transformChildren(node, mergedOptions)
+        children: transformChildren(node, replacements)
       };
     }
   });
 };
 
-const transformChildren = (node: SvgNode, options: ResetColorsPluginOptions) =>
+const transformChildren = (node: SvgNode, replacements: ColorPropertyReplacement[]) =>
   Array.isArray(node.children)
-    ? node.children.map(child => transformElement(child, options))
+    ? node.children.map(child => transformElement(child, replacements))
     : node.children;
 
-const transformElement = (node: SvgNode, options: ResetColorsPluginOptions): SvgNode => ({
+const transformElement = (node: SvgNode, replacements: ColorPropertyReplacement[]): SvgNode => ({
   ...node,
-  children: transformChildren(node, options),
+  children: transformChildren(node, replacements),
   attributes: {
     ...node.attributes,
-    ...getResetColorAttributes(node.attributes, options)
+    ...getResetColorAttributes(node.attributes, replacements)
   }
 });
 
 const getResetColorAttributes = (
   target: Record<string, string>,
-  { includeProperties, includeValues, replaceTo }: ResetColorsPluginOptions
-) =>
-  Object.fromEntries(
-    includeProperties
-      .filter(name => Boolean(target[name]) && includeValues.includes(target[name]))
-      .map(name => [name, replaceTo])
-  );
+  replacements: ColorPropertyReplacement[]
+) => {
+  const result = { ...target };
 
-const defaults: ResetColorsPluginOptions = {
-  includeProperties: ['fill', 'stroke'],
-  includeValues: ['#000', '#000000'],
-  replaceTo: 'currentColor'
+  for (const { properties, replaceUnknown, replace } of replacements) {
+    for (const name of properties.filter(name => name in target)) {
+      const value = target[name];
+      const replacement =
+        replace.find(({ from }) => from.some(color => color.isEqual(value)))?.to ?? replaceUnknown;
+
+      result[name] = replacement ?? result[name];
+      console.log('reset', name, value, replacement);
+    }
+  }
+  return result;
 };
+
+const defaults = {
+  properties: ['fill', 'stroke'],
+  replace: {
+    from: ['#000', '#000000'],
+    to: 'currentColor'
+  }
+} satisfies ColorPropertyReplacementInput;
+
+interface ColorPropertyReplacement {
+  properties: string[];
+  replace: ColorReplacement[];
+  replaceUnknown?: string;
+}
+
+interface ColorReplacement {
+  from: Colord[];
+  to: string;
+}
