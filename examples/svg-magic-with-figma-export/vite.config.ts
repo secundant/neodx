2769a85c4eccdcdffd6787d4b53794c3979fcba2
Dmitry Remezov <mysecundant@gmail.com>
2023-05-24 11:38:27 +0300

    docs(svg): rework example with figma integration

--- examples/svg-magic-with-figma-export/vite.config.ts
+++ examples/svg-magic-with-figma-export/vite.config.ts
@@ -1,9 +1,23 @@
 /// <reference types="vitest" />
+// eslint-disable-next-line import/no-unresolved
+import svg from '@neodx/svg/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: { typecheck: { ignoreSourceErrors: true }, passWithNoTests: true },
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svg({
+      root: 'assets/icons',
+      output: 'public',
+      group: true,
+      resetColors: {
+        replace: ['#6C707E', '#A8ADBD', '#818594']
+      },
+      definitions: 'src/shared/ui/icon/sprite.gen.ts'
+    })
+  ]
 });
