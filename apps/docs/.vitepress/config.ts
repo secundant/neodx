de418c5d74176b7996512fb48c9a770b8cdffbe2
Dmitry Remezov <mysecundant@gmail.com>
2023-08-20 16:02:32 +0300

    docs: add favicons, choose theme colors

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -4,7 +4,19 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Neodx',
   description: 'Modern solutions for great DX',
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#710ab7' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#603cba' }],
+    ['meta', { name: 'msapplication-config', content: '/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ],
   themeConfig: {
+    logo: '/logo.png',
     socialLinks: [{ icon: 'github', link: 'https://github.com/secundant/neodx' }],
     footer: {
       message: 'Released under the MIT License.',
