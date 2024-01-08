0679ab4137290d8a7616656be3aad68563ee9fe0
Dmitry Remezov <mysecundant@gmail.com>
2024-01-08 23:50:30 +0400

    feat(glob): rework `walkGlob`

--- libs/glob/src/walk.ts
+++ libs/glob/src/walk.ts
@@ -1,56 +1,125 @@
-import { combineAbortSignals, False, isTypeOfFunction, uniqBy } from '@neodx/std';
+import {
+  combineAbortSignals,
+  every,
+  False,
+  identity,
+  isTruthy,
+  isTypeOfFunction,
+  prop,
+  uniqBy
+} from '@neodx/std';
+import { join } from 'node:path';
 import { extractGlobPaths } from './extract.ts';
 import { createGlobMatcher } from './match.ts';
 
-export interface WalkGlobParams<Item> {
+export interface WalkGlobParams<Item, Result> extends WalkGlobCommonParams {
+  /**
+   * Reads base path and returns content of that.
+   * By default, expected to return a list of all path's descendants.
+   * @example
+   * walkGlob('**\/*.ts', {
+   *   reader: ({ path }) => readdir(path, { recursive: true })
+   * })
+   */
+  reader: (params: WalkReaderParams) => Item[] | Promise<Item[]>;
+  /**
+   * Should return a path relative to the glob.
+   * @default identity - returns the item as is, expecting it to be a path
+   */
+  mapPath?: (item: Item, params: WalkReaderParams) => string;
+  /**
+   * Converts collected items to the desired result (it could be not just a path).
+   * @default join(path, item) - returns the full relative path
+   */
+  mapResult?: (item: Item, params: WalkReaderParams) => Result;
+}
+
+/**
+ * Common params could be used in top-level APIs around walkGlob.
+ */
+export interface WalkGlobCommonParams {
   timeout?: number;
   ignore?: WalkIgnoreInput;
   signal?: AbortSignal;
-
-  reader: (params: WalkReaderParams) => Item[] | Promise<Item[]>;
-  getItemPath: (item: Item) => string;
 }
 
+/**
+ * Params for the reader and mapResult functions.
+ * You can think about it as a context for the one of the glob's path.
+ */
 export interface WalkReaderParams {
+  /**
+   * One of the base paths extracted for the glob.
+   * Could be empty string if the glob is relative.
+   * @example Single base path
+   * glob: 'src/*.ts'
+   * path: 'src'
+   * @example Multiple base paths
+   * glob: 'src/{foo,bar}/*.ts'
+   * path: 'src/foo', path: 'src/bar'
+   * @example Relative glob with no base path
+   * glob: '**\/*.ts'
+   * path: ''
+   */
   path: string;
+  /**
+   * Accepts a path relative to the glob.
+   * Returns true if the path matches the glob and not ignored.
+   */
+  match: WalkIgnore;
   signal: AbortSignal;
-  isIgnored: WalkIgnore;
-  isMatched: WalkIgnore;
 }
 
 export type WalkIgnoreInput = WalkIgnore | RegExp | string | string[];
 export type WalkIgnore = (path: string) => boolean;
 
-export async function walkGlob<Item>(
+interface InternalCollectedItem<Item, Result> {
+  path: string;
+  item: Item;
+  result: Result;
+}
+
+export async function walkGlob<Item, Result = string>(
   glob: string | string[],
-  { ignore = False, signal: customSignal, timeout, reader, getItemPath }: WalkGlobParams<Item>
+  {
+    ignore = False,
+    signal: customSignal,
+    timeout,
+    reader,
+    mapPath = identity as any,
+    mapResult
+  }: WalkGlobParams<Item, Result>
 ) {
   const isIgnored = createIgnoreChecker(ignore);
-  const collected: Item[] = [];
+  const collected: InternalCollectedItem<Item, Result>[] = [];
   const signal = combineAbortSignals([customSignal, timeout && AbortSignal.timeout(timeout)]);
 
   for (const [path, patterns] of extractGlobPaths(glob)) {
-    signal.throwIfAborted();
     if (isIgnored(path)) continue;
-    const isMatched = createGlobMatcher(patterns);
-    const items = await reader({
-      path,
-      signal,
-      isIgnored,
-      isMatched
-    });
+    signal.throwIfAborted();
+    const toFullPath = (subPath: string) => join(path, subPath);
+    const itemToResult = mapResult ?? (item => toFullPath(mapPath(item, params)) as Result);
+    const isNotIgnoredSubPath = (subPath: string) => !isIgnored(toFullPath(subPath));
+    const match = every(isNotIgnoredSubPath, createGlobMatcher(patterns));
+    const params = { path, match, signal };
+    const allFoundItems = await reader(params);
 
     collected.push(
-      ...items.filter(item => {
-        const itemPath = getItemPath(item);
-
-        return !isIgnored(itemPath) && isMatched(itemPath);
-      })
+      ...allFoundItems
+        .map(
+          item =>
+            match(mapPath(item, params)) && {
+              item,
+              path: toFullPath(mapPath(item, params)),
+              result: itemToResult(item, params)
+            }
+        )
+        .filter(isTruthy)
     );
   }
 
   signal.throwIfAborted();
-  return uniqBy(collected, getItemPath);
+  return uniqBy(collected, item => item.path).map(prop('result'));
 }
 
 export const createIgnoreChecker = (ignore: WalkIgnoreInput) => {
