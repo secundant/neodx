9b3adfcad3b884979961c207e6e8d5f4328bcff3
Dmitry Remezov <mysecundant@gmail.com>
2024-01-09 22:56:13 +0400

    feat(std): add `memoize`

--- libs/std/src/memoize.ts
+++ libs/std/src/memoize.ts
@@ -1,6 +1,21 @@
 export function memoizeWeak<Input extends object, Output>(fn: (input: Input) => Output) {
-  const cache = new WeakMap<Input, Output>();
+  return memoize(fn, {
+    cache: new WeakMap()
+  });
+}
 
+export function memoize<Input, Output>(
+  fn: (input: Input) => Output,
+  {
+    cache = new Map<Input, Output>()
+  }: {
+    cache?: {
+      has(input: Input): boolean;
+      get(input: Input): Output | undefined;
+      set(input: Input, value: Output): void;
+    };
+  } = {}
+) {
   return (input: Input) => {
     if (!cache.has(input)) {
       cache.set(input, fn(input));
